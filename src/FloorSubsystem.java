ccc8274bd80d1069c97c4b1151c13aba94eeb17f
Abdelrahman Darwish <abdel_rahman1991@hotmail.com>
2020-01-28 14:45:40 -0500

    Made Changes

--- src/FloorSubsystem.java
+++ src/FloorSubsystem.java
@@ -5,8 +5,33 @@ public class FloorSubsystem implements Runnable{
 	private void run(){
 		
 	}
+	/**
+	 * TimeStamp Method 
+	 * @return Returns the at which the elevator what called/pressed
+	 */
+	private String TimeStamp() {
+		return myArray[0];
+	}
+	/**
+	 * 
+	 * @returns Floor number 
+	 */
+	private String FloorNumber() {
+		return myArray[1];
+	}
+	
+	private String UpOrDown() {
+		return myArray[2];
+	}
+	
+	private String ButtonPressed() {
+		return myArray[3];
+	}
+
+	
 	
 	public static void main(String[] args) {	
+		
 		List<String>inputData = new ArrayList<String>();
 	        try {
 	        	Scanner scanner = new Scanner(new File("C:/Users/abdel/Onedrive/Desktop/inputFile.csv"));
@@ -17,7 +42,7 @@ public class FloorSubsystem implements Runnable{
 	 	        	inputData.add(x);
 	 	        }
 	 	        scanner.close();
-	 	        String[] myArray = inputData.toArray(new String[0]);
+	 	        String[] myArray = inputData.toArray(new String[0]);  
 	 	        for (String s: myArray) {
 	 	        	System.out.println(s);
 	 	        }
@@ -25,8 +50,8 @@ public class FloorSubsystem implements Runnable{
 	        catch(FileNotFoundException e) {
 	        	e.printStackTrace();
 	        	
-	        }
-	        }
+	        	 }
+	    }
 }
 		
 	
