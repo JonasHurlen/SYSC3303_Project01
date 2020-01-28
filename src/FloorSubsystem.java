30309c8ce36730e3b192c73469c332d4f6b89871
Abdelrahman Darwish <abdel_rahman1991@hotmail.com>
2020-01-28 14:33:35 -0500

    Open File logic completed and parsing csv file

--- src/FloorSubsystem.java
+++ src/FloorSubsystem.java
@@ -1,35 +1,33 @@
 import java.io.*;
+import java.util.*;
 public class FloorSubsystem implements Runnable{
 		
 	private void run(){
 		
 	}
 	
-	public static void main(String[] args) {
-		String csvFile = "";
-		BufferedReader buffreader=null;
-		String cvsSplitby =",";
-		
-		
-		try{
-			
-			buffreader= new BufferedReader(new FileReader(csvFile));
-			while() {
-				
-			}
-			
-			
-			
-		}
-		catch(FileNotFoundException e){
-			e.printStackTrace();
-			
-		}
-		finally{
-			
-		}
-	}
-		
-	}
-
+	public static void main(String[] args) {	
+		List<String>inputData = new ArrayList<String>();
+	        try {
+	        	Scanner scanner = new Scanner(new File("C:/Users/abdel/Onedrive/Desktop/inputFile.csv"));
+	 	        scanner.useDelimiter(","); //sets the delimiter pattern
+	 	        
+	 	        while(scanner.hasNext()) {
+	 	        	String x=scanner.next();
+	 	        	inputData.add(x);
+	 	        }
+	 	        scanner.close();
+	 	        String[] myArray = inputData.toArray(new String[0]);
+	 	        for (String s: myArray) {
+	 	        	System.out.println(s);
+	 	        }
+	        }
+	        catch(FileNotFoundException e) {
+	        	e.printStackTrace();
+	        	
+	        }
+	        }
 }
+		
+	
+
