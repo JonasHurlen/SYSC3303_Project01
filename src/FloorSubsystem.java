c86e15031a7f160de2660521053a4ab3463a0d4c
Abdelrahman Darwish <abdel_rahman1991@hotmail.com>
2020-01-31 16:11:15 -0500

    Made a function instead of main

--- src/FloorSubsystem.java
+++ src/FloorSubsystem.java
@@ -29,31 +29,38 @@ public class FloorSubsystem implements Runnable{
 	private String ButtonPressed() {
 		return myArray[3];
 	}
+	
+	/**
+	 * 
+	 * @param filename
+	 */
+	
+	public void readInputFile(String filename) {
+		
+		List<String>inputData = new ArrayList<String>();
+        try {
+        	Scanner scanner = new Scanner(new File(filename));
+ 	        scanner.useDelimiter(","); //sets the delimiter pattern
+ 	        
+ 	        while(scanner.hasNext()) {
+ 	        	String x=scanner.next();
+ 	        	inputData.add(x);
+ 	        }
+ 	        scanner.close();
+ 	        String[] myArray = inputData.toArray(new String[0]);  
+ 	        for (String s: myArray) {
+ 	        	System.out.println(s);
+ 	        }
+        }
+        catch(FileNotFoundException e) {
+        	e.printStackTrace();
+        	
+        	 }
+	}
 
 	
 	
-	public static void main(String[] args) {	
-		
-		List<String>inputData = new ArrayList<String>();
-	        try {
-	        	Scanner scanner = new Scanner(new File("inputFile.csv"));
-	 	        scanner.useDelimiter(","); //sets the delimiter pattern
-	 	        
-	 	        while(scanner.hasNext()) {
-	 	        	String x=scanner.next();
-	 	        	inputData.add(x);
-	 	        }
-	 	        scanner.close();
-	 	        String[] myArray = inputData.toArray(new String[0]);  
-	 	        for (String s: myArray) {
-	 	        	System.out.println(s);
-	 	        }
-	        }
-	        catch(FileNotFoundException e) {
-	        	e.printStackTrace();
-	        	
-	        	 }
-	    }
+	
 }
 		
 	
