0e9682410c74605eb79813e09ef0281a83639252
Abdelrahman Darwish <abdel_rahman1991@hotmail.com>
2020-02-01 19:37:03 -0500

    Improved Documentation for FloorSubsytem

--- src/FloorSubsystem.java
+++ src/FloorSubsystem.java
@@ -29,31 +29,38 @@ public class FloorSubsystem implements Runnable {
 		}
 
 	}
-
+/**
+ * FloorSubsystem Constructor
+ * @param scheduler
+ * @param floorNumber
+ */
 	public FloorSubsystem(Scheduler scheduler, int floorNumber) {
 		this.scheduler = scheduler;
 		this.floorNumber = floorNumber;
 	}
 	
 	/**
-	 * 
-	 * @param filename
+	 * readInputFile() method will read in events from csv file to be sent to the scheduler 
+	 * @param String that is the name of the csv file
+	 * @return Instruction 
 	 */
 
 	public Instruction readInputFile(String filename) {
 		Instruction instruction = null;
 		List<String> inputData = new ArrayList<String>();
 		try {
+			// Create a new scanner that takes in the csv file
 			Scanner scanner = new Scanner(new File(filename));
 			scanner.useDelimiter(","); // sets the delimiter pattern
-
+            // Checks if the scanner object has another token, if there is then it will add it to the list
 			while (scanner.hasNext()) {
 				String x = scanner.next();
 				inputData.add(x);
 			}
+			//Close the Scanner
 			scanner.close();
 			String[] myArray = inputData.toArray(new String[0]);
-
+            //Creating a new Instruction
 			instruction = new Instruction(Integer.parseInt(myArray[1]), Integer.parseInt(myArray[2]));
 
 		} catch (FileNotFoundException e) {
