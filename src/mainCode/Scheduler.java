13ec962eaf48265d0cc3b5e5e4fb275e214f1339
Jonas H <jonashurlen@cmail.carleton.ca>
2020-02-15 17:26:47 -0500

    Established Framework for Centralized Console

--- src/mainCode/Scheduler.java
+++ src/mainCode/Scheduler.java
@@ -33,12 +33,12 @@ public class Scheduler implements Runnable {
 				if (!this.inputF.isEmpty()) {
 					// Takes information from floor and sends it through to the elevators to receive
 					// info
-					if(inputF.peek().getFloorBut() == 0) {
-						System.out.println(inputF.peek().getFloor() + " going down");
-					}else {
-						System.out.println(inputF.peek().getFloor() + " going up");
+					if (inputF.peek().getFloorBut() == 0) {
+						console(inputF.peek().getFloor() + " going down");
+					} else {
+						console(inputF.peek().getFloor() + " going up");
 					}
-					
+
 					outputE.add(inputF.pop());
 
 				}
@@ -49,7 +49,7 @@ public class Scheduler implements Runnable {
 					Instruction order = inputE.pop();
 					switch (order.getType()) {
 					case 0:
-						System.out.println("S C0");
+						console("S C0");
 						int distance = 999;
 						cars = order.getCarPoll();
 						for (Car car : cars) {
@@ -59,7 +59,7 @@ public class Scheduler implements Runnable {
 									order.setCarNum(car.getId());
 									outputE.add(order);
 								}
-								//checks to see nearest car to the call, will be optimized later
+								// checks to see nearest car to the call, will be optimized later
 								if (distance < Math.abs(car.getCurrFloor() - order.getFloor())) {
 									distance = Math.abs(car.getCurrFloor() - order.getFloor());
 									order.setCarNum(car.getId());
@@ -70,25 +70,25 @@ public class Scheduler implements Runnable {
 						}
 						if (order.getFloor() > order.getCarCur()) {
 							order.setMove(1);
-						} else if(order.getFloor() < order.getCarCur()){
+						} else if (order.getFloor() < order.getCarCur()) {
 							order.setMove(-1);
 						}
-						System.out.println(order.getMove());
+						console(order.getMove());
 						this.outputE.add(order);
 						break;
 
 					case 1:
-						System.out.println("S C1");
+						console("S C1");
 						int currCar = order.getCarNum();
 						if (orders[currCar] == null) {
 							orders[currCar] = new LinkedList<Instruction>();
 						}
 						order.setType(2);
 						orders[currCar].add(order);// fit new destination into schedule
-						System.out.println(order.getMove());
+						console(order.getMove());
 						break;
 					case 2:
-						System.out.println("S C2");
+						console("S C2");
 						if (!orders[order.getCarNum()].isEmpty()) {
 							if (order.getCarBut() == order.getCarCur()) {
 								this.acknowledged.add(order);
@@ -101,9 +101,9 @@ public class Scheduler implements Runnable {
 								order.setMove(-1);
 								this.outputE.add(order);
 							}
-							
+
 						}
-						System.out.println(order.getMove());
+						console(order.getMove());
 						break;
 
 					}
@@ -115,6 +115,10 @@ public class Scheduler implements Runnable {
 
 		}
 
+	}
+
+	public void console(Object in) {
+		System.out.println(in);
 	}
 
 }
